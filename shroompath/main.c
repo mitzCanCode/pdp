edfd5f8e983ae2626ee104a02ae5a30c7fd9aaa1
Dimitris Chatzigeorgiou <dimitris.chatzi@proton.me>
2025-02-20 11:54:56 +0400

    Update main.c

--- shroompath/main.c
+++ shroompath/main.c
@@ -4,62 +4,81 @@
 //
 //  Created by Dimitris Chatzigeorgiou on 16/2/25.
 //
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdlib.h>
 
-long int total_combinations(long int num) {
-    long int p = 1;
-    for (int i = 0; i<num; i++) {
-        p = 2 * p;
-    }
-    return p;
-}
- 
-void generate_combinations(long int n, char *current, char result[][100], int *index) {
-    if (strlen(current) == n) {
-        printf("%s\n", current);
-        strcpy(result[(*index)++], current);
+void generate_combinations(long int combo_length, char *current, char **result, int *index) {
+    if (strlen(current) == combo_length) {
+        strcpy(result[(*index)++], current); // Store in result array
         return;
     }
  
     char next[100];
+ 
     strcpy(next, current);
     strcat(next, "a");
-    generate_combinations(n, next, result, index);
+    generate_combinations(combo_length, next, result, index);
  
     strcpy(next, current);
     strcat(next, "b");
-    generate_combinations(n, next, result, index);
+    generate_combinations(combo_length, next, result, index);
 }
  
-int main(void) {
-    long int total_combos = 2;
-    long int combo_length = 1;  // Change this value as needed
+char **get_combinations(long int combo_length, long int *total_combos) {
+    *total_combos = 1 << combo_length; // 2^n possible combinations
+    char **result = (char **)malloc((*total_combos) * sizeof(char *));
+    for (int i = 0; i < *total_combos; i++) {
+        result[i] = (char *)malloc((combo_length + 1) * sizeof(char));
+    }
+ 
     int index = 0;
-    int s = 13;
-    int x = 5;
-    int y = 3;
-    int basket = 0;
-    int steps = 0;
-    char basket_content = 'a';
+    char start[100] = ""; // Start with an empty string
+    generate_combinations(combo_length, start, result, &index);
+ 
+    return result;
+}
+ 
+void print_results(char **result, long int total_combos, long int combo_length) {
+    for (int i = 0; i < total_combos; i++) {
+        printf("%s\n", result[i]);
+    }
+}
+ 
+void free_results(char **result, int total_combos) {
+    for (int i = 0; i < total_combos; i++) {
+        free(result[i]);
+    }
+    free(result);
+}
+
+
+ 
+int main() {
+    long int combo_length = 1;  
+    int s = 10;
+    int x = 2;
+    int y = 5;
+    long int total_combos;
+    long int steps;
     bool done = false;
-    
-    while (basket < s) {
-        // Calculating the total number of combinations that will be created
-        total_combos = total_combinations(combo_length);
-        char combinations[total_combos][combo_length];
-        
+    char basket_content = 'a';
+	long int basket;
+ 	
+
+ 
+ 
+ 	while (basket < s) {
         // Creating a new longer set of combinations
-        generate_combinations(combo_length, "", combinations, &index);
-        for (int i= 0; i<total_combos+1; i++){
-            for (int j = 0; j<combo_length; j++){
-                printf("%c", &combinations[i][j]);
-            }
-        }
-        steps = 0;
-        printf("Generated new combination\n");
+    	char **combinations = get_combinations(combo_length, &total_combos);
+
+        // steps = 0;
+        printf("Generated new combinations:\n");
+        print_results(combinations, total_combos, combo_length);
+
         // Iterating over the combinations
         long int i = 0;
         while (i<total_combos && !done) {
@@ -100,56 +119,13 @@ int main(void) {
             printf("here\n");
             i++;
         }
-
+		free_results(combinations, total_combos);
         combo_length++;
-        index = 0;
-
     }
+ 	
     long int result = steps % ((long int)pow(10,9) + 7);
     printf("%ld", result);
+
+ 
     return 0;
 }
-
-
-
-//int main(void) {
-//    char test[4][2] = {"aa", "ab", "ba", "bb"};
-//    int combos = 4;
-//    int length = 2;
-//    for (int i = 0; i<combos; i++) {
-//        for (int j = 0; j<length; j++) {
-//            printf("%c", test[i][j]);
-//        }
-//    }
-//    return 0;
-//}
-
-
-//        for (long int i = 0; i<total_combos; i++) {
-//            printf("Currently testing combination: %s", combinations[i]);
-//            // Iterating over each character of a combination
-//            for (long int j = 0; j<combo_length; j++) {
-//                printf("Currently testing for %c\n", combinations[i][j]);
-//                // Checking if the mushroom changes
-//                if (basket_content != combinations[i][j]) {
-//                    // Changing the type of shrooms in the basket to the current one
-//                    basket_content = combinations[i][j];
-//                    printf("Basket content removed switching to %c\n", basket_content);
-//                    // Reseting the basket
-//                    basket = 0;
-//                }
-//                // Adding the correct mushroom weight to the basket
-//                if (basket_content == 'a') {
-//                    basket += x;
-//                    printf("Added a to basket and increased by: %d\n", x);
-//
-//                } else {
-//                    basket += y;
-//                    printf("Added b to basket and increased by: %d\n", y);
-//                }
-//                printf("Current basket size: %d\n", basket);
-//
-//                // Increasing the steps done
-//                steps++;
-//            }
-//        }
